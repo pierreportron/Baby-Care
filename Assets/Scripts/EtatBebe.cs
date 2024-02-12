f116739d824001436ab5e28d2aedfb6d640e1bca
Pierre Portron <pportron@etud.univ-angers.fr>
2024-02-12 19:55:54 +0100

    connaitre état bébé

---
+++ Assets/Scripts/EtatBebe.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EtatBebe : MonoBehaviour
+{
+    private GameObject bebe;
+    public int etat;
+    public float position;
+    public bool inBed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bebe = gameObject;
+        inBed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inBed)
+        {
+
+            if (convertir(bebe.transform.localRotation.eulerAngles.x) >= -100 && convertir(bebe.transform.localRotation.eulerAngles.x) <= -80)
+            {
+                etat = 0;
+            }
+            else if (convertir(bebe.transform.localRotation.eulerAngles.x) >= -110 && convertir(bebe.transform.localRotation.eulerAngles.x) <= -70)
+            {
+                etat = 1;
+            }
+            else
+            {
+                etat = 2;
+            }
+        }
+        else
+        {
+            etat = 3;
+        }
+        position=convertir(bebe.transform.localRotation.eulerAngles.x);
+    }
+    private float convertir(float angle)
+    {
+        if (angle > 180)
+        {
+            return angle - 360;
+        }
+        return angle;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bed")
+        {
+            inBed = true;
+            Debug.Log("inBed");
+        }
+    }
+        private void OnTriggerExit(Collider other)
+    {
+        if ( other.gameObject.tag == "Bed")
+        {
+            inBed = false;
+        }
+    }
+    public int getEtat()
+    {
+        return etat;
+    }   
+}
