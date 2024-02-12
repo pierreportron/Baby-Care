fec4390506c6d04987e9a6cd2896a66e8da7876a
Pierre Portron <pportron@etud.univ-angers.fr>
2024-02-12 17:55:22 +0100

    correction info

--- Assets/Scripts/ChangeImageSommeil.cs
+++ Assets/Scripts/ChangeImageSommeil.cs
@@ -8,28 +8,37 @@ public class ChangeImageSommeil : MonoBehaviour
 {
     public Texture image1;
     public Texture image2;
-    public GameObject text;
-    public GameObject buttontext;
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI buttontext;
     public GameObject rawimage;
-    public int state=0;
+    public int state;
     // Start is called before the first frame update
-    void changeImage()
+    public void changeImage()
     {
         if (state==0)
         {
             rawimage.GetComponent<RawImage>().texture = image2;
-            text.GetComponent<TextMeshPro>().text = "Tenez le bébé à deux mains"; 
-            buttontext.GetComponent<Text>().text = "Précédent"; 
+            //text.GetComponent<TextMeshProUGUI>().text = "Tenez le bébé à deux mains"; 
+            //buttontext.GetComponent<TextMeshProUGUI>().text ="Précédent"; 
+            text.text = "Déposez le bébé dans son lit, sur le dos"; 
+            buttontext.text ="Précédent"; 
             state=1;
 
         }
         else
         {
             rawimage.GetComponent<RawImage>().texture = image1; 
+            //text.GetComponent<TextMeshProUGUI>().text ="Déposez le bébé dans son lit, sur le dos"; 
+            //buttontext.GetComponent<TextMeshProUGUI>().text="Suivant"; 
+            text.text ="Tenez le bébé à deux mains"; 
+            buttontext.text="Suivant"; 
             state=0;
-            text.GetComponent<TextMeshPro>().text = "Déposez le bébé dans son lit, sur le dos"; 
-            buttontext.GetComponent<Text>().text = "Suivant"; 
 
         }
     }
+    void Start()
+    {
+        state=0;
+    }
+
 }
